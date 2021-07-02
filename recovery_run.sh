6a8a5ca7822403d3432c5f5da3af464dec7fad28
System Administrator <root@MacBook-Pro.fritz.box>
2021-07-02 13:05:03 +0000

    new release
    Committer: Romain Gayon <rgayon@MacBook-Pro.fritz.box>

--- recovery_run.sh
+++ recovery_run.sh
@@ -61,8 +61,11 @@ function prepare_env() {
   if [[ ! -f "${PREFIX}/bin/python3" ]]; then
     echo "Preparing necessary environment"
 
+    mkdir -p "${PREFIX}"
+
     if [[ "${TARBALLURL}" == "" ]]; then
       echo "please specify a URL to pull the environment from with --url"
+      echo "example: run_recovery.sh --url https://storage.cloudapis.com/<bucket>/<path>/minithon.tgz"
       exit 1
     fi
 
@@ -72,9 +75,6 @@ function prepare_env() {
 
     tarball="${TARBALLURL##*/}"
     echo "Downloaded ${tarball}."
-    shasum "${tarball}"
-    echo "Is this correct? (ctrl-c) if not"
-    read
 
     # In the recovery environment, /opt is actually a symlink to /System/Volumes/Data/opt
     tar -C /System/Volumes/Data -x -z -f "${tarball}"
@@ -85,7 +85,7 @@ function prepare_env() {
     pip3 install certifi
     # Making sure we have the default local trusted CA store in the path expected by python ssl module
     sslpem="$(python3 -c 'import ssl; print(ssl.get_default_verify_paths().openssl_cafile)')"
-    certifipem="$(python3 -c 'import certifi; print(s=certifi.where())')"
+    certifipem="$(python3 -c 'import certifi; print(certifi.where())')"
     if [[ ! -f "${sslpem}" ]]; then
       echo "Python SSL library expects ${sslpem} to be present. Adding it from ${certifipem}"
       mkdir -p "$(dirname ${sslpem})"
@@ -104,14 +104,15 @@ function prepare_giftstick() {
     git clone https://github.com/google/GiftStick
 
     pushd GiftStick
+    echo "installing gift dependencies"
+    pip3 install -r requirements.txt
+
     echo "Patching boto lib"
     # apply boto patches because of https://github.com/boto/boto/pull/3699
     boto_dir=$(python3.9 -c "import boto; print(boto.__path__[0])")
     patch -p0 "${boto_dir}/connection.py" config/patches/boto_pr3561_connection.py.patch
     patch -p0 "${boto_dir}/s3/key.py" config/patches/boto_pr3561_key.py.patch
 
-    echo "installing gift dependencies"
-    pip3 install -r requirements.txt
     popd
     popd
   fi
@@ -147,9 +148,14 @@ prepare_env
 check_env
 prepare_giftstick
 
-echo "Everything is ready! You can run the acquisition code, ie:"
-echo "PYTHONPATH=. python auto_forensicate/auto_acquire.py --gs_keyfile sa.json --acquire directory gs://<remotebucket>/"
-echo ""
-echo "Spawning a new shell in ${WORKDIR}/GiftStick, with PATH containing $PREFIX"
+echo
+echo
+echo "Everything is ready!!! You can run the acquisition code, ie:"
+echo
+echo "PYTHONPATH=. python3 auto_forensicate/auto_acquire.py --gs_keyfile sa.json --acquire directory gs://<remotebucket>/"
+echo
 echo "see https://github.com/google/GiftStick for more information"
+echo
+echo "Spawning a new shell in ${WORKDIR}/GiftStick, with $PATH containing $PREFIX"
+cd "${WORKDIR}"/GiftStick
 $SHELL -i
